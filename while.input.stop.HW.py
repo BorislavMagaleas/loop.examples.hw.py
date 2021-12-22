61262cabd19493dd3d10b55f59e9a4230209486f
BorislavMagaleas <95872181+BorislavMagaleas@users.noreply.github.com>
2021-12-22 17:31:33 +0200

    Assignment was completed.

---
+++ while.input.stop.HW.py
@@ -0,0 +1,5 @@
+while True:
+    print("BOO YA!")
+    answer = input("Do you want me to stop?")   ### INPUT. User is required to introduce data that can either 
+    if answer == "yes!":                        ### Verifies whether the value that stops the cycle is introduced
+        break
